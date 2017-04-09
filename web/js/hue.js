b18b56338b127d67c3f065b1a8e3701e6af03414
jeffyactive <jeff@reelyactive.com>
2017-04-09 17:07:07 -0400

    Connect to Hue bridge

--- web/js/hue.js
+++ web/js/hue.js
@@ -15,6 +15,16 @@ angular.module('hue', [ 'ui.bootstrap' ])
       });
     };
 
+    $scope.connectToBridge = function(ip) {
+      $http.defaults.headers.common.Accept = 'application/json';
+      $http({ method: 'PUT', url: '/hue/bridges', data: { ip: ip } })
+        .then(function(response) {
+          $scope.hue.bridges = response.data;
+        }, function(response) {
+          $scope.hue.bridges = {};
+      });
+    };
+
     $scope.getState();
 
   });
