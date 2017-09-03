04f372c3851d65c5079a11cf886645a50c98377e
jeffyactive <jeff@reelyactive.com>
2017-09-03 11:34:22 -0400

    Added socket.io midi message view

--- web/js/midi.js
+++ web/js/midi.js
@@ -1,9 +1,25 @@
+/**
+ * Copyright reelyActive 2017
+ * We believe in an open Internet of Things
+ */
+
+
+const MIDI_EVENTS_HISTORY = 5;
+
+
+/**
+ * midi Module
+ */
 angular.module('midi', [ 'ui.bootstrap' ])
 
-  // Hue controller
-  .controller('MidiCtrl', function($scope, $http, $interval) {
+  // Midi controller
+  .controller('MidiCtrl', function($scope, $http, $location) {
+    var socket = connectSocket();
+    socket.on('midi', handleMessage);
 
+    // Variables accessible in the HTML scope
     $scope.midi = {};
+    $scope.events = [];
 
     $scope.getInterfaces = function() {
       $http.defaults.headers.common.Accept = 'application/json';
@@ -24,6 +40,23 @@ angular.module('midi', [ 'ui.bootstrap' ])
           $scope.midi.interfaces = {};
       });
     };
+
+    function connectSocket() {
+      var url = $location.protocol() + '://' + $location.host() + ':' +
+                $location.port();
+      return io.connect(url);
+    }
+
+    function handleMessage(message) {
+      if(message.type === 'message') {
+        $scope.events.push(message.data);
+        if($scope.events.length > MIDI_EVENTS_HISTORY) {
+          $scope.events.shift();
+        }
+        console.log(message.data);
+      }
+      $scope.$apply();
+    }
 
     $scope.getInterfaces();
 
