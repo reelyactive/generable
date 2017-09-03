d2f47fdf10b9597e43e274d3a67b4cbc5374a817
jeffyactive <jeff@reelyactive.com>
2017-09-03 10:50:06 -0400

    Added real-time reel events view

--- web/js/reel.js
+++ web/js/reel.js
@@ -1,9 +1,25 @@
+/**
+ * Copyright reelyActive 2017
+ * We believe in an open Internet of Things
+ */
+
+
+const REEL_EVENTS_HISTORY = 5;
+
+
+/**
+ * reel Module
+ */
 angular.module('reel', [ 'ui.bootstrap' ])
 
-  // Hue controller
-  .controller('ReelCtrl', function($scope, $http, $interval) {
+  // Reel controller
+  .controller('ReelCtrl', function($scope, $http, $location) {
+    var socket = connectSocket();
+    socket.on('reel', handleMessage);
 
-    $scope.hue = {};
+    // Variables accessible in the HTML scope
+    $scope.reel = {};
+    $scope.events = [];
 
     $scope.getState = function() {
       $http.defaults.headers.common.Accept = 'application/json';
@@ -14,6 +30,23 @@ angular.module('reel', [ 'ui.bootstrap' ])
           $scope.reel = {};
       });
     };
+
+    function connectSocket() {
+      var url = $location.protocol() + '://' + $location.host() + ':' +
+                $location.port();
+      return io.connect(url);
+    }
+
+    function handleMessage(message) {
+      if(message.type === 'visibility') {
+        $scope.events.push(message.data);
+        if($scope.events.length > REEL_EVENTS_HISTORY) {
+          $scope.events.shift();
+        }
+        console.log(message.data);
+      }
+      $scope.$apply();
+    }
 
     $scope.getState();
 
