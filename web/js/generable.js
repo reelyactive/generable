6ee2953ef94e8c3b8799e3f1b8e9670252d45a48
jeffyactive <jeff@reelyactive.com>
2017-09-10 11:26:52 -0400

    Added scene activation

--- web/js/generable.js
+++ web/js/generable.js
@@ -1,1 +1,22 @@
-angular.module('generable', [ 'ui.bootstrap' ]);
+/**
+ * Copyright reelyActive 2017
+ * We believe in an open Internet of Things
+ */
+
+
+// Add new scenes here and they will be presented on the webpage
+const SCENES = [
+  { name: "Square Shades", url: "scenes/squareshades/" }
+];
+
+
+/**
+ * generable Module
+ */
+angular.module('generable', [ 'ui.bootstrap' ])
+
+  // Display controller
+  .controller('DisplayCtrl', function($scope) {
+    $scope.scenes = SCENES;
+    $scope.selectedScene = SCENES[0].name;
+  });
