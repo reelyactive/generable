7d3b81a7a13d01fc8dcc563be2fd46e16c48b809
jeffyactive <jeff@reelyactive.com>
2018-07-18 19:25:55 -0400

    Added MIDI assignments feature

--- web/js/midi.js
+++ web/js/midi.js
@@ -1,10 +1,55 @@
 /**
- * Copyright reelyActive 2017
+ * Copyright reelyActive 2017-2018
  * We believe in an open Internet of Things
  */
 
 
 const MIDI_EVENTS_HISTORY = 5;
+
+const SOURCES = [
+  'none',
+  'decoding',
+  'appearance',
+  'displacement',
+  'disappearance'
+];
+
+const NOTES = [
+  'Drum Kit',
+  'C maj',
+  'C min',
+  'D maj',
+  'D min',
+  'E maj',
+  'E min',
+  'F maj',
+  'F min',
+  'G maj',
+  'G min',
+  'A maj',
+  'A min',
+  'B maj',
+  'B min',
+  'C maj (triad)',
+  'D maj (triad)',
+  'E maj (triad)',
+  'F maj (triad)',
+  'G maj (triad)',
+  'A maj (triad)',
+  'B maj (triad)'
+];
+
+const DURATIONS = [
+  '1/16',
+  '1/8',
+  '1/4',
+  '1/2',
+  '1',
+  '2',
+  '4',
+  '8',
+  '16'
+];
 
 
 /**
@@ -18,7 +63,8 @@ angular.module('midi', [ 'ui.bootstrap' ])
     socket.on('midi', handleMessage);
 
     // Variables accessible in the HTML scope
-    $scope.midi = {};
+    $scope.midi = { assignments: [], sources: SOURCES, notes: NOTES,
+                    durations: DURATIONS };
     $scope.events = [];
 
     $scope.getInterfaces = function() {
@@ -41,6 +87,29 @@ angular.module('midi', [ 'ui.bootstrap' ])
       });
     };
 
+    $scope.getAssignments = function() {
+      $http.defaults.headers.common.Accept = 'application/json';
+      $http({ method: 'GET', url: '/midi/assignments' })
+        .then(function(response) {
+          $scope.midi.assignments = response.data;
+        }, function(response) {
+          $scope.midi.assignments = [];
+      });
+    };
+
+    $scope.updateAssignment = function(channel, assignment) {
+      var url = '/midi/assignments/' + channel;
+      var data = { assignment: assignment };
+
+      $http.defaults.headers.common.Accept = 'application/json';
+      $http({ method: 'PUT', url: url, data: data })
+        .then(function(response) {
+          // All good
+        }, function(response) {
+          console.log('Assignment update failed.');
+      });
+    };
+
     function connectSocket() {
       var url = $location.protocol() + '://' + $location.host() + ':' +
                 $location.port();
@@ -58,5 +127,6 @@ angular.module('midi', [ 'ui.bootstrap' ])
     }
 
     $scope.getInterfaces();
+    $scope.getAssignments();
 
   });
